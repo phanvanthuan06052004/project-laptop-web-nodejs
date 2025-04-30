850dada818bbed5e30d9bb74ff093bc74c16f955
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-04-30 21:21:24 +0700

    feat: jwt provider

--- backend/src/providers/JwtProvider.js
+++ backend/src/providers/JwtProvider.js
@@ -1,1 +1,34 @@
-// code here
+import JWT from 'jsonwebtoken'
+
+const generateToken = (payload, secretSignature, tokenLife) => {
+  try {
+    return JWT.sign(payload, secretSignature, { algorithm: 'HS256', expiresIn: tokenLife })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+//Hàm verify của thư viện này sẽ trả về payload nếu đúng
+const verifyToken = async (token, secretSignature) => {
+  try {
+    const decodedPayload = JWT.verify(token, secretSignature)
+    return decodedPayload // Return the decoded payload from verify
+  } catch (error) {
+    return error
+  }
+}
+
+const decodeToken = (token) => {
+  try {
+    const decoded = JWT.decode(token)
+    return decoded
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const JwtProvider = {
+  generateToken,
+  verifyToken,
+  decodeToken
+}
