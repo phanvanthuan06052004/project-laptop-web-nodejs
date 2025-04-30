2d70d7bb6857d7950402587aa2b1d3716bdd1a42
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-05-01 00:01:43 +0700

    feat: initial user controller and route

---
+++ backend/src/routes/v1/userRoute.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import { userController } from '~/controllers/userController'
+import { authMiddlewares } from '~/middlewares/authMiddeware'
+import { userValidation } from '~/validations/userValidation'
+import { uploadRoute } from './uploadRoute'
+
+
+const Router = express.Router()
+
+// API to register account
+Router.route('/auth/register')
+  .post(userValidation.createNew, userController.createNew)
+
+// api to login
+Router.route('/auth/')
+  .post(userValidation.signIn, userController.signIn)
+
+// api handle refresh token
+Router.route('/auth/refresh-token')
+  .post(userValidation.refreshToken, userController.refreshToken)
+
+// api to upload image
+Router.use('/upload', uploadRoute)
+
+// api get user profile, update user profile
+Router.route('/profile')
+  .get(authMiddlewares.authentication, userController.getUserProfile)
+  .put(authMiddlewares.authentication, userValidation.updateUser, userController.updateUser)
+
+// api to logout
+Router.route('/auth/logout')
+  .delete(authMiddlewares.authentication, userController.logout)
+
+// get all user
+Router.route('/')
+  .get(authMiddlewares.authentication, userValidation.getAll, userController.getAll)
+
+// get detail, update, and delete user
+Router.route('/:id')
+  .put(userValidation.updateUserByUserId, userController.updateUserByUserId)
+  .get(userValidation.getUserById, userController.getUserById)
+  .delete(authMiddlewares.authentication, authMiddlewares.authorization, userValidation.deleteAccount, userController.deleteAccount)
+
+
+export const userRoute = Router
