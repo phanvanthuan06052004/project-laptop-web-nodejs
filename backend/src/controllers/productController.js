aa8fb8f5c12c8cd9b612257ef1d0e1ea86e5808a
zerrorTwo <namle022004@gmail.com>
2025-05-01 22:54:06 +0700

    crud product

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,75 @@
+import { StatusCodes } from 'http-status-codes'
+import { productService } from '~/services/productService'
+
+const getAll = async (req, res, next) => {
+    try {
+        const result = await productService.getAll(req.query)
+        res.status(StatusCodes.OK).json(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const createNew = async (req, res, next) => {
+    try {
+        const newProduct = await productService.createNew(req.body)
+        res.status(StatusCodes.CREATED).json(newProduct)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getProductById = async (req, res, next) => {
+    try {
+        const result = await productService.getProductById(req.params.id)
+        res.status(StatusCodes.OK).json(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const updateProduct = async (req, res, next) => {
+    try {
+        const result = await productService.updateProduct(req.params.id, req.body)
+        res.status(StatusCodes.OK).json(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const deleteProduct = async (req, res, next) => {
+    try {
+        const result = await productService.deleteProduct(req.params.id)
+        res.status(StatusCodes.OK).json(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getProductByNameSlug = async (req, res, next) => {
+    try {
+        const result = await productService.getProductByNameSlug(req.params.nameSlug)
+        res.status(StatusCodes.OK).json(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getPageProduct = async (req, res, next) => {
+    try {
+        const result = await productService.getPageProduct(req.query)
+        res.status(StatusCodes.OK).json(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const productController = {
+    getAll,
+    createNew,
+    getProductById,
+    updateProduct,
+    deleteProduct,
+    getProductByNameSlug,
+    getPageProduct
+}
