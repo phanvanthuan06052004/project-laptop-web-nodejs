0789903ed6d47196a83d02978279d058e4cdc967
zerrorTwo <namle022004@gmail.com>
2025-05-10 22:26:24 +0700

    fix auth

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -8,21 +8,21 @@ import jwt from 'jsonwebtoken'
 const authentication = (async (req, res, next) => {
   const userId = req?.headers[HEADER.CLIENT_ID]
   if (!userId) {
-    throw new ApiError(StatusCodes.UNAUTHORIZED, 'Not found userId')
+    next( new ApiError(StatusCodes.UNAUTHORIZED, 'Not found userId'))
   }
 
   const bearerAccessToken = req?.headers[HEADER.AUTHORIZATION]
   const accessToken = bearerAccessToken?.split(' ')[1]
 
   if (!accessToken) {
-    throw new ApiError(StatusCodes.UNAUTHORIZED, 'Invalid access token')
+    next ( new ApiError(StatusCodes.UNAUTHORIZED, 'Invalid access token') )
   }
 
   try {
     // Synchronously verify access token
     const decodedUser = await JwtProvider.verifyToken(accessToken, env.ACCESS_TOKEN_SECRET_SIGNATURE)
     if (userId?.toString() !== decodedUser?.id?.toString()) {
-      throw new ApiError(StatusCodes.UNAUTHORIZED, 'Token does not match')
+      next ( new ApiError(StatusCodes.UNAUTHORIZED, 'Token does not match'))
     }
     req.userId = decodedUser?.id
     req.userRole = decodedUser?.role
@@ -43,7 +43,7 @@ const authorization = (async (req, res, next) => {
     if (req?.userRole === 'admin') {
       return next()
     } else {
-      throw new ApiError(StatusCodes.FORBIDDEN, 'Access denied')
+      next(new ApiError(StatusCodes.FORBIDDEN, 'Access denied'))
     }
   } catch (error) {
     next(error)
